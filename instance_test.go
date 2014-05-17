89419eee49aa717126ae02b37b28e9fde1557f62
Robert Obryk <robryk@gmail.com>
2014-05-17 12:52:19 +0200

    added a test for instance

--- instance_test.go
+++ instance_test.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"testing"
+	"time"
 )
 
+var catBinary string
 var trueBinary string
+var falseBinary string
 
 func init() {
 	for _, s := range []string{"/bin/true"} {
@@ -15,30 +20,75 @@ func init() {
 			break
 		}
 	}
+	for _, s := range []string{"/bin/false"} {
+		if _, err := os.Stat(s); err == nil {
+			falseBinary = s
+			break
+		}
+	}
+	for _, s := range []string{"/bin/cat"} {
+		if _, err := os.Stat(s); err == nil {
+			catBinary = s
+			break
+		}
+	}
 }
 
-func startInstance(cmd *exec.Cmd) (*Instance, error) {
+func startInstance(t *testing.T, cmd *exec.Cmd) *Instance {
 	instances := make([]*Instance, 1)
 	var err error
 	instances[0], err = NewInstance(cmd, 0, instances)
 	if err != nil {
-		return nil, err
+		t.Fatalf("Error creating an instance for %s: %v", cmd.Path, err)
 	}
 	if err := instances[0].Start(); err != nil {
-		return nil, err
+		t.Fatalf("Error starting an instance for %s: %v", cmd.Path, err)
 	}
-	return instances[0], nil
+	return instances[0]
 }
 
 func TestInstanceSuccess(t *testing.T) {
 	if trueBinary == "" {
-		t.Skipf("No /bin/true equivalent found")
+		t.Skip("No /bin/true equivalent found")
 	}
-	instance, err := startInstance(exec.Command(trueBinary))
-	if err != nil {
-		t.Fatalf("Error creating an instance for %s: %v", trueBinary, err)
-	}
+	instance := startInstance(t, exec.Command(trueBinary))
 	if err := instance.Wait(); err != nil {
 		t.Fatalf("Error running %s: %v", trueBinary, err)
+	}
+}
+
+func TestInstanceFailure(t *testing.T) {
+	if falseBinary == "" {
+		t.Skip("No /bin/false equivalent found")
+	}
+	instance := startInstance(t, exec.Command(falseBinary))
+	if err := instance.Wait(); err == nil {
+		t.Fatalf("No error when running %s", falseBinary)
+	}
+}
+
+func TestInstanceKill(t *testing.T) {
+	if catBinary == "" {
+		t.Skip("No /bin/cat equivalent found")
+	}
+	cmd := exec.Command(catBinary)
+	r, _ := io.Pipe()
+	defer r.Close()
+	cmd.Stdin = r
+	cmd.Stdout = ioutil.Discard
+	instance := startInstance(t, cmd)
+	waitChan := make(chan error)
+	go func() {
+		waitChan <- instance.Wait()
+	}()
+	// The instance shouldn't finish of its own accord
+	select {
+	case err := <-waitChan:
+		t.Fatalf("/bin/cat has finished prematurely, err=%v", err)
+	case <-time.After(100 * time.Millisecond):
+	}
+	instance.Kill()
+	if err := <-waitChan; err != ErrKilled {
+		t.Errorf("A killed instance has finished with error %v, instead of %v", err, ErrKilled)
 	}
 }
