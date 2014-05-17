b5dff51fb0254831a7ea0df91f3069a7751220f4
Robert Obryk <robryk@gmail.com>
2014-05-17 02:05:00 +0200

    Instance: fixed ordering during startup, handle more errors

--- instance.go
+++ instance.go
@@ -12,6 +12,9 @@ type Instance struct {
 	instances []*Instance
 	cmd       *exec.Cmd
 
+	stdinCopier          func()
+	communicateGoroutine func()
+
 	messagesSent     int
 	messageBytesSent int
 
@@ -23,6 +26,14 @@ type Instance struct {
 
 // TODO: errors, communicate later, queues
 func NewInstance(cmd *exec.Cmd, id int, instances []*Instance) (*Instance, error) {
+	instance := &Instance{
+		id:        id,
+		instances: instances,
+		cmd:       cmd,
+		queues:    make([]*MessageQueue, len(instances)),
+		selector:  make(chan *MessageQueue),
+		errChan:   make(chan error, 3),
+	}
 	if cmd.Stdin != nil {
 		stdin := cmd.Stdin
 		cmd.Stdin = nil
@@ -30,9 +41,17 @@ func NewInstance(cmd *exec.Cmd, id int, instances []*Instance) (*Instance, error
 		if err != nil {
 			log.Fatal(err)
 		}
-		go func() {
-			io.Copy(stdinPipe, stdin) // XXX errors?
-		}()
+		instance.stdinCopier = func() {
+			defer stdinPipe.Close()
+			_, err := io.Copy(stdinPipe, stdin)
+			if err != nil {
+				select {
+				case instance.errChan <- err:
+				default:
+				}
+				instance.cmd.Process.Kill()
+			}
+		}
 	}
 	cmdr, cmdw, err := os.Pipe()
 	if err != nil {
@@ -43,21 +62,12 @@ func NewInstance(cmd *exec.Cmd, id int, instances []*Instance) (*Instance, error
 		return nil, err
 	}
 	cmd.ExtraFiles = []*os.File{respr, cmdw}
-	instance := &Instance{
-		id:        id,
-		instances: instances,
-		cmd:       cmd,
-		queues:    make([]*MessageQueue, len(instances)),
-		selector:  make(chan *MessageQueue),
-		errChan:   make(chan error, 1),
-	}
 	for i := range instance.queues {
 		// XXX teardown
 		instance.queues[i] = NewMessageQueue(instance.selector)
 	}
 
-	go func() {
-		// XXX after init
+	instance.communicateGoroutine = func() {
 		if err := instance.communicate(cmdr, respw); err != nil {
 			select {
 			case instance.errChan <- err:
@@ -67,7 +77,7 @@ func NewInstance(cmd *exec.Cmd, id int, instances []*Instance) (*Instance, error
 		}
 		cmdr.Close()
 		respw.Close()
-	}()
+	}
 	return instance, nil
 }
 
@@ -81,6 +91,11 @@ func (i *Instance) Run() error {
 			return err
 		}
 	}
+
+	if i.stdinCopier != nil {
+		go i.stdinCopier()
+	}
+	go i.communicateGoroutine()
 	go func() {
 		select {
 		case i.errChan <- i.cmd.Wait():
@@ -88,10 +103,6 @@ func (i *Instance) Run() error {
 		}
 	}()
 	return <-i.errChan
-}
-
-func (i *Instance) Start() error {
-	return i.cmd.Start()
 }
 
 func (i *Instance) Kill() error {
