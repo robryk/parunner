5f81588ce18701594c209c089f3ec3504cc2f654
Robert Obryk <robryk@gmail.com>
2014-05-19 18:30:17 +0200

    fix a race between PutMessage and possible ShutdownQueues

--- instances.go
+++ instances.go
@@ -16,31 +16,28 @@ func (ie InstanceError) Error() string {
 	return fmt.Sprintf("Błąd instancji %d: %v", ie.ID, ie.Err)
 }
 
-// RunInstances starts one instance for each command in cmds, waits for
-// them to terminate and returns the instances themselves and the first
-// error that has occurred. The instances returned are always valid,
-// even if the error is non-nil. Each command in cmds is started, even
-// if some of them fail to start. The error returned is always an InstanceError
-// that indicated which instance caused the error. If an error occurs, all
-// the rest of the instances are terminated early.
+// RunInstances starts each command from cmds in an Instance and
+// waits either for all of them to finish successfully or for
+// the first error. In the latter case, all the rest of
+// the instances are killed. All the instances are then returned
+// in the slice. RunInstances additionally guarantees the following:
+// * The instance slice is valid even if the error is non-nil
+// * All the commands have been started before RunInstances returns
+// * All the instanced have been waited on before RunInstances returns
+// * The error returned is an instance of InstanceError and contains
+//   the instance ID that caused the error.
 func RunInstances(cmds []*exec.Cmd) ([]*Instance, error) {
-	messagesCh := make(chan Message, 1)
 	var wg sync.WaitGroup
+	defer wg.Wait()
 
-	defer func() {
-		wg.Wait()
-		// This channel has to be closed _after_ all the instances have terminated,
-		// because it's written to by the instances when they send a message.
-		// Thus, we close it only after observing that wg is empty.
-		close(messagesCh)
-	}()
-
+	messagesCh := make(chan Message, 1)
 	results := make(chan error, 1)
 	is := make([]*Instance, len(cmds))
 	for i, cmd := range cmds {
 		var err error
 		is[i], err = StartInstance(cmd, i, len(cmds), messagesCh)
 		if err != nil {
+			is[i] = nil
 			select {
 			case results <- InstanceError{i, err}:
 			default:
@@ -61,16 +58,18 @@ func RunInstances(cmds []*exec.Cmd) ([]*Instance, error) {
 		}(i, is[i])
 	}
 	go func() {
-		for m := range messagesCh {
-			is[m.Target].PutMessage(m)
-		}
-	}()
-	go func() {
 		wg.Wait()
 		select {
 		case results <- nil:
 		default:
 		}
 	}()
-	return is, <-results
+	for {
+		select {
+		case m := <-messagesCh:
+			is[m.Target].PutMessage(m)
+		case err := <-results:
+			return is, err
+		}
+	}
 }
