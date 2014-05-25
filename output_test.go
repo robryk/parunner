81483d6d705e450bb30559110661eb4dea36687a
Robert Obryk <robryk@gmail.com>
2014-05-25 02:44:54 +0200

    added a test for ContestStdout

--- output_test.go
+++ output_test.go
@@ -2,9 +2,47 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 	"strings"
+	"sync"
 	"testing"
 )
+
+func TestContestStdout(t *testing.T) {
+	var buf bytes.Buffer
+	cs := &ContestStdout{Output: &buf}
+
+	const N = 10
+	var wg sync.WaitGroup
+	token := make(chan bool, 1)
+	token <- true
+	expectedOutput := make(chan string, 1)
+	for i := 0; i < N; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			s := fmt.Sprintf("%d\nfoobarbazblah", i)
+			w := cs.NewWriter(i)
+			_, err := io.Copy(w, strings.NewReader(s))
+			if err != nil {
+				return
+			}
+			select {
+			case <-token:
+			default:
+				t.Errorf("instance %d was also able to write", i)
+				return
+			}
+			expectedOutput <- s
+		}(i)
+	}
+	want := <-expectedOutput
+	got := buf.String()
+	if got != want {
+		t.Errorf("wrong output of ContestStdout: got=%q, want=%q", got, want)
+	}
+}
 
 func TestTagStream(t *testing.T) {
 	for _, tc := range []struct {
