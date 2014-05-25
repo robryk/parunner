a796e44fb4cc08aff648d36371c920bd9bd5a339
Robert Obryk <robryk@gmail.com>
2014-05-25 19:03:50 +0200

    add accidentally forgotten tests for instances

---
+++ instances_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestInstances(t *testing.T) {
+	testerBinary := filepath.Join("zeus", "tester")
+	if _, err := os.Stat(testerBinary); err != nil {
+		t.Skipf("%s not found", testerBinary)
+	}
+	type testcase struct {
+		name             string
+		inputs           []string
+		expectedOutputs  []string // only used if len(expectedFails) == 0
+		expectedFails    []int    // if empty, we expect success; otherwise we expect failure from one of the numerated instances
+		expectedDeadlock bool
+	}
+	testcases := []*testcase{
+		{"no comm", []string{"", "", "", ""}, []string{"0 4\n", "1 4\n", "2 4\n", "3 4\n"}, nil, false},
+		{"fail", []string{"", "Q 1", "Q 2"}, nil, []int{1, 2}, false},
+		{"send with no recv", []string{"", "Safoo\n"}, []string{"0 2\n", "1 2\n"}, nil, false},
+		{"send with recv", []string{"Rb\n", "Safoo\n"}, []string{"0 2\n1 3 foo\n", "1 2\n"}, nil, false},
+		{"send with recvany", []string{"R*\n", "Safoo\n"}, []string{"0 2\n1 3 foo\n", "1 2\n"}, nil, false},
+		{"deadlock", []string{"Scfoo\nR*\n", "R*\n", ""}, nil, nil, true},
+		{"fail and hang", []string{"H\n", "Q 1\n"}, nil, []int{1}, false},
+		{"fail and hanging recv", []string{"R*\n", "Q 1\n"}, nil, []int{1}, false},
+	}
+	for _, tc := range testcases {
+		outputs := make([]bytes.Buffer, len(tc.inputs))
+		cmds := make([]*exec.Cmd, len(tc.inputs))
+		for i, input := range tc.inputs {
+			cmds[i] = exec.Command(testerBinary)
+			cmds[i].Stdin = strings.NewReader(input)
+			cmds[i].Stdout = &outputs[i]
+		}
+		_, err := RunInstances(cmds, ioutil.Discard)
+		if _, ok := err.(ErrRemainingMessages); ok {
+			err = nil
+		}
+		if err != nil {
+			switch err := err.(type) {
+			case InstanceError:
+				ok := false
+				for _, i := range tc.expectedFails {
+					if err.ID == i {
+						ok = true
+					}
+				}
+				if !ok {
+					t.Errorf("test %s: unexpected error from RunInstances: %v", tc.name, err)
+				}
+			case ErrDeadlock:
+				if !tc.expectedDeadlock {
+					t.Errorf("test %s: unexpected deadlock", tc.name)
+				}
+			default:
+				t.Errorf("test %s: unexpected error from RunInstances: %v", tc.name, err)
+			}
+			continue
+		}
+		if len(tc.expectedFails) != 0 || tc.expectedDeadlock {
+			t.Errorf("test %s: unexpected success of RunInstances", tc.name)
+			continue
+		}
+		for i, want := range tc.expectedOutputs {
+			got := outputs[i].String()
+			if got != want {
+				t.Errorf("test %s: wrong output from instance %d: got=%q, want=%q", tc.name, i, got, want)
+			}
+		}
+	}
+}
+
+// TODO: check what happens when we send/recv message to/from an instance that doesn't exist
+// TODO: check what happens when an instance claims that its CPU time goes backward
